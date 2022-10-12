2f15171bc8ef5cdb489a65ebfddacafe8e09bc56
Shinhhwan <kangshinhwan26@gmail.com>
2022-10-12 19:18:57 +0900

    qq
    qq

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
 
 # Configuration file for the Sphinx documentation builder.
 
