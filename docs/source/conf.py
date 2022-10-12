eb89ddc83cd01f2b6331e8fe409095e432059f52
Shinhhwan <kangshinhwan26@gmail.com>
2022-10-12 19:28:51 +0900

    ww
    ww

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
-sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath('..'))
-sys.path.append(os.path.abspath('../..'))
+sys.path.append(os.path.abspath('./'))
+sys.path.append(os.path.abspath('../'))
+sys.path.append(os.path.abspath('../../'))
 
 # Configuration file for the Sphinx documentation builder.
 
